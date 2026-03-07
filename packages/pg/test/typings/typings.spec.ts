0117844fd92025b699f849ae2bc5abee6022d3cb
fgcoelho <fernando@bunnyware.com>
2026-03-07 13:08:47 -0300

    test(pg): simplify typings definition and remove unused tests

--- packages/pg/test/typings/typings.spec.ts
+++ packages/pg/test/typings/typings.spec.ts
@@ -1,22 +1,22 @@
 import { describe, expect, it } from "vitest";
-import type { Typings } from "../../src/index";
+import type { CreateTypings } from "../../src";
+
+type Typings = CreateTypings<{
+  public: {
+    user: {
+      id: any;
+      name: any;
+      role: any;
+    };
+    post: {
+      id: any;
+      title: any;
+    };
+  };
+}>;
 
 describe("Typings", () => {
   it("allows custom typings with autocompletion", () => {
-    type MyTypings = Typings<{
-      public: {
-        user: {
-          id: any;
-          name: any;
-          role: any;
-        };
-        post: {
-          id: any;
-          title: any;
-        };
-      };
-    }>;
-
     const customTypings = {
       public: {
         user: {
@@ -29,26 +29,11 @@ describe("Typings", () => {
           },
         },
       },
-    } satisfies MyTypings;
+    } satisfies Typings;
 
     expect(customTypings.public?.user?.role).toHaveProperty("~standard");
     expect((customTypings as any).public?.user?.id).toBeUndefined();
     expect((customTypings as any).public?.user?.name).toBeUndefined();
     expect((customTypings as any).public?.post).toBeUndefined();
-  });
-
-  it("allows empty object to satisfy Typings", () => {
-    type MyTypings = Typings<{
-      public: {
-        user: {
-          id: any;
-          email: any;
-        };
-      };
-    }>;
-
-    const result = {} satisfies MyTypings;
-
-    expect(result).toEqual({});
   });
 });
